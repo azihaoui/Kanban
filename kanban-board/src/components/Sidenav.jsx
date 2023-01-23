7f0bb81736d0c53fdb650d0ef60fe3ee04906625
Rusman Steele <azihaoui@hotmail.com>
2023-01-23 11:17:47 +0100

    added sidebar

--- kanban-board/src/components/Sidenav.jsx
+++ kanban-board/src/components/Sidenav.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import './SideNav'
+import './SideNav/Sidenav.scss'
 
 function Sidenav() {
   return (
     <div className='sidenav'>
         <p>ALL BOARDS (2)</p>
+        <ul>
+            <li>Kanban board</li>
+        </ul>
     </div>
   )
 }
