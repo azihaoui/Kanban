7d0317b62f759fba15d26a8df9ada9e240b97beb
Rusman Steele <azihaoui@hotmail.com>
2023-01-23 10:41:24 +0100

    init

---
+++ kanban-board/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidenav from "./components/Sidenav"
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Sidenav />
+    </div>
+  )
+}
+
+export default App
---
+++ kanban-board/src/components/Sidenav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SideNav'
+
+function Sidenav() {
+  return (
+    <div className='sidenav'>
+        <p>ALL BOARDS (2)</p>
+    </div>
+  )
+}
+
+export default Sidenav
